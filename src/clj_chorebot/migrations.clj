f42a1aace872dee87ff70298dff34324226d3e82
Jeff Chen <jchen@ladderlife.com>
2018-01-23 17:17:46 -0800

    add migration

--- src/clj_chorebot/migrations.clj
+++ src/clj_chorebot/migrations.clj
@@ -2,7 +2,8 @@
   (:require [clj-chorebot.migrations.init :as init]
             [clj-chorebot.migrations.add-users :as add-users]
             [clj-chorebot.migrations.add-chores :as add-chores]
-            [clj-chorebot.migrations.add-chorelogs :as add-chorelogs]))
+            [clj-chorebot.migrations.add-chorelogs :as add-chorelogs]
+            [clj-chorebot.migrations.add-topic :as add-topic]))
 
 (defn migrate []
   (do
@@ -11,4 +12,5 @@
     (add-users/migrate)
     (add-chores/migrate)
     (add-chorelogs/migrate)
+    (add-topic/migrate)
     (println "migrations completed!")))
---
+++ src/clj_chorebot/migrations/add_topic.clj
@@ -0,0 +1,24 @@
+(ns clj-chorebot.migrations.add-topic
+  (:require [clojure.java.jdbc :as sql]
+            [clj-chorebot.config :as config]
+            [clojure.string :as str]
+            [clj-chorebot.slack :as slack]
+            [clj-chorebot.models.chorelog :as chorelog]
+            [clj-chorebot.models.user :as user]))
+
+(def migration-name "add-users")
+
+(defn migrated? []
+  (-> (sql/query config/db-url
+                 [(str "select count(*) from migrations "
+                       "where name='" migration-name "'")])
+      first :count pos?))
+
+(defn migrate []
+  (when (not (migrated?))
+    (print "Setting topic...") (flush)
+    (let [all-chores (map #(do {:slack-handle (:slack-handle (user/get-next-user (:chore-order %)))
+                                :chore-name (:name %)}) (chorelog/get-last-all))]
+      (slack/set-topic config/chores-channel (str/join " | " (map #(format "%s: %s" (:chore-name %) (:slack-handle %)) all-chores))))
+    (sql/insert! config/db-url :migrations {:name migration-name})
+    (println " done")))
